115bdda060fdfa5c818c3f4595c4be0a0e8fd4fb
Aleksandr Tolmachev <a.tolmachev@xsolla.com>
2019-11-08 00:24:54 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask import request
+import requests
+from flask import make_response
+import os
+import json
+from pandas import DataFrame
+import traceback
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def on_event():
+  """Handles an event from Hangouts Chat."""
+  event = request.get_json()
+  if event['type'] == 'ADDED_TO_SPACE' and event['space']['type'] == 'ROOM':
+    text = 'Thanks for adding me to "%s"!' % event['space']['displayName']
+  elif event['type'] == 'MESSAGE':
+    text = 'You said: `%s`' % event['message']['text']
+  else:
+    return
+  return json.jsonify({'text': text})
+
+
+
+
+
+
+if __name__ == "__main__":
+    port = int(os.getenv('PORT', 5000))
+    app.run(debug=False, port=port, host='0.0.0.0')
+
