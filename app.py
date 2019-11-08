268a32a6279967cbb21c1ba5d5f4ef1935feaf34
Aleksandr Tolmachev <a.tolmachev@xsolla.com>
2019-11-08 11:11:41 +0500

    fix

--- app.py
+++ app.py
@@ -1,32 +1,70 @@
-from flask import Flask
-from flask import request
-import requests
-from flask import make_response
-import os
-import json
-from pandas import DataFrame
-import traceback
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# pylint: disable=invalid-name
+"""
+Simple Hangouts Chat bot that responds to events and
+messages from a room.
+"""
+# [START basic-bot]
+import logging
+from flask import Flask, render_template, request, json
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
-def on_event():
-  """Handles an event from Hangouts Chat."""
-  event = request.get_json()
-  if event['type'] == 'ADDED_TO_SPACE' and event['space']['type'] == 'ROOM':
-    text = 'Thanks for adding me to "%s"!' % event['space']['displayName']
-  elif event['type'] == 'MESSAGE':
-    text = 'You said: `%s`' % event['message']['text']
-  else:
-    return
-  return json.jsonify({'text': text})
+def home_post():
+    """Respond to POST requests to this endpoint.
+    All requests sent to this endpoint from Hangouts Chat are POST
+    requests.
+    """
 
+    data = request.get_json()
 
+    resp = None
 
+    if data['type'] == 'REMOVED_FROM_SPACE':
+        logging.info('Bot removed from a space')
 
+    else:
+        resp_dict = format_response(data)
+        resp = json.jsonify(resp_dict)
+
+    return resp
+
+def format_response(event):
+    """Determine what response to provide based upon event data.
+    Args:
+      event: A dictionary with the event data.
+    """
+
+    text = ""
+
+    # Case 1: The bot was added to a room
+    if event['type'] == 'ADDED_TO_SPACE' and event['space']['type'] == 'ROOM':
+        text = 'Thanks for adding me to "%s"!' % event['space']['displayName']
+
+    # Case 2: The bot was added to a DM
+    elif event['type'] == 'ADDED_TO_SPACE' and event['space']['type'] == 'DM':
+        text = 'Thanks for adding me to a DM, %s!' % event['user']['displayName']
+
+    elif event['type'] == 'MESSAGE':
+        text = 'Your message: "%s"' % event['message']['text']
+
+    return {'text': text}
 
 
 if __name__ == "__main__":
     port = int(os.getenv('PORT', 5000))
     app.run(debug=False, port=port, host='0.0.0.0')
-
